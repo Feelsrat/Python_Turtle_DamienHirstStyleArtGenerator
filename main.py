cd601c532c5557e3ce908e3719fae7ece78db6dc
Feelsrat <40744868+Feelsrat@users.noreply.github.com>
2022-01-24 14:33:20 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import random
 import colorgram
 import turtle as t
 
+amount_of_colours_extracted = 10  # how many colours to extract from image.jpg
+number_of_dots = 400  # total amount of dots in the image. This will affect how tall the image will be
+spacing_between_dots = 25  # space between dots both vert and horiz
+
 rgb_colors = []
-colors = colorgram.extract('image.jpg', 6)
+colors = colorgram.extract('image.jpg', amount_of_colours_extracted)
 for color in colors:
     r = color.rgb.r
     g = color.rgb.g
@@ -12,7 +16,9 @@ for color in colors:
     rgb_colors.append(new_color)
 
 print(rgb_colors)
+
 turtle = t.Turtle()
+turtle.hideturtle()
 t.colormode(255)
 turtle.penup()
 turtle.speed(0)
@@ -20,19 +26,16 @@ turtle.setheading(225)
 turtle.forward(350)
 turtle.setheading(0)
 
-number_of_dots = 100
-
 for dot_count in range(1, number_of_dots + 1):
     turtle.dot(20, random.choice(rgb_colors))
-    turtle.forward(50)
+    turtle.forward(spacing_between_dots)
 
-    if dot_count % 10 == 0:
+    if dot_count % 20 == 0:
         turtle.setheading(90)
-        turtle.forward(50)
+        turtle.forward(spacing_between_dots)
         turtle.setheading(180)
         turtle.forward(500)
         turtle.setheading(0)
 
-turtle.hideturtle()
 screen = t.Screen()
 screen.exitonclick()
